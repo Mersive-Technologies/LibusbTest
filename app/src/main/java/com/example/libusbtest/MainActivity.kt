3c967011c00e6bd3738935f91f11f10e6a065cb2
Brent Gardner <bgardner@squarelabs.net>
2020-09-12 11:43:03 -0600

    Minimal passing test

--- app/src/main/java/com/example/libusbtest/MainActivity.kt
+++ app/src/main/java/com/example/libusbtest/MainActivity.kt
@@ -1,23 +1,36 @@
 package com.example.libusbtest
 
+import android.content.Context
+import android.hardware.usb.UsbDeviceConnection
+import android.hardware.usb.UsbManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val tag: String = this.javaClass.name
+    private var usbManager: UsbManager? = null
+    private val devices = mutableMapOf<Int, UsbDeviceConnection>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Example of a call to a native method
+        usbManager = getSystemService(Context.USB_SERVICE) as UsbManager
+        for(dev in usbManager!!.deviceList.values) {
+            Log.i(tag, "Found vid=${dev.vendorId} pid=${dev.productId}")
+            if (dev.vendorId == 0x046d && dev.productId == 0x0866) {
+                Log.i(tag, "Found meetup!")
+                val con = usbManager!!.openDevice(dev)
+                devices[dev.deviceId] = con
+                Log.i(tag, "Opened meetup: fd=${con.fileDescriptor}")
+            }
+        }
+
         sample_text.text = stringFromJNI()
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
     external fun stringFromJNI(): String
 
     companion object {
