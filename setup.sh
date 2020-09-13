dc40d6bee698f3651d053057443a684470309ed2
Brent Gardner <bgardner@squarelabs.net>
2020-09-13 13:43:42 -0600

    Fix panic?

--- setup.sh
+++ setup.sh
@@ -12,17 +12,11 @@ export DEBIAN_FRONTEND="noninteractive"
 export ANDROID_HOME="$HOME/Android/Sdk"
 
 # updates
-apt list --installed | grep jdk
-apt list --installed | grep jre
-java -version
 sudo apt-get -qq remove -y openjdk*
-apt list --installed | grep jdk
-apt list --installed | grep jre
-java -version
-
 sudo apt-get -qq update -y
 sudo apt-get -qq install -y linux-tools-$(uname -r) linux-modules-extra-$(uname -r) openjdk-8-jdk \
   unzip wget curl git python python3 cmake build-essential pkg-config libusb-1.0-0-dev
+java -version
 
 # usbip
 find /lib/modules/ -name usbip*
@@ -40,7 +34,7 @@ echo "setup env"
 mkdir -p $ANDROID_HOME
 export ANDROID_NDK_HOME=${ANDROID_HOME}/ndk/${NDK_VERSION}
 export TOOLCHAIN=${ANDROID_NDK_HOME}/toolchains/llvm/prebuilt/linux-x86_64
-export PATH=${TOOLCHAIN}/bin:${ANDROID_HOME}/cmake/${CMAKE_VERSION}/bin:${ANDROID_HOME}/tools:${ANDROID_HOME}/tools/bin:${ANDROID_HOME}/platform-tools:$PATH
+export PATH=${ANDROID_SDK}/emulator:${TOOLCHAIN}/bin:${ANDROID_HOME}/cmake/${CMAKE_VERSION}/bin:${ANDROID_HOME}/tools:${ANDROID_HOME}/tools/bin:${ANDROID_HOME}/platform-tools:$PATH
 
 echo "Downloading and installing the Android SDK"
 mkdir -p $HOME/.ssh/
